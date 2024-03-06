e5cfbfd9b6f542cedbb07f137685f7a8d7097c13
alfaeridani <115158881+alfaeridani@users.noreply.github.com>
2024-03-06 22:31:05 +0700

    Create a function that makes custom sized grid based on input from user

--- etchasketch.js
+++ etchasketch.js
@@ -1,12 +1,30 @@
 const container = document.getElementById("container");
+const squareNumber = document.getElementById("squareNumber");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+squareNumber.addEventListener('click', () => {
+    height = prompt("How tall you want the grid to be? (Max. 100)", "20");
+    width = prompt("How tall you want the grid to be? (Max. 100)", "20");
+    createNewGrid(height, width);
+});
+
+function createNewGrid(height, width) {
+    for (let i = 0; i < height; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < width; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    const squares = document.getElementsByClassName("square")
+    const squaresArray = Array.from(squares);
+
+    squaresArray.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "grey";
+        });
+    });
 }
+
