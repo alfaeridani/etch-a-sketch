f838e2cc845fde890ad9a8ac03efde4b584feed4
alfaeridani <115158881+alfaeridani@users.noreply.github.com>
2024-03-06 23:30:14 +0700

    Modify UI and some text

--- etchasketch.js
+++ etchasketch.js
@@ -5,12 +5,12 @@ squareNumber.addEventListener('click', () => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    height = prompt("How tall you want the grid to be? (1-100)", "20");
-    width = prompt("How tall you want the grid to be? (1-100)", "20");
+    height = prompt("How tall? (1-100)", "20");
+    width = prompt("How wide? (1-100)", "20");
     while (height > 100 || width > 100 || height < 1 || width < 1) {
         alert("Height and width must be higher than 0 and lower than 100.");
-        height = prompt("How tall you want the grid to be? (Max. 100)", "20");
-        width = prompt("How tall you want the grid to be? (Max. 100)", "20");
+        height = prompt("How tall? (Max. 100)", "20");
+        width = prompt("How wide? (Max. 100)", "20");
     }
     createNewGrid(height, width);
 });
